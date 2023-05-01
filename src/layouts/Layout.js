1c159a6d5578e5452bbf8157100719908ef5f4b0
yujunglove <dbwjdeor1234562@gmail.com>
2023-05-01 17:50:14 +0900

    commit

---
+++ src/layouts/Layout.js
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/common/Navbar";
+import Navbar2 from "../components/common/Navbar2";
+import Footer from "../components/common/Footer";
+
+function Layout() {
+
+    return (
+        <>
+            <Navbar/>
+            <Navbar2/>
+            <Outlet/>
+            <Footer/>
+        </>
+    );
+}
+
+export default Layout;
