bcef40e2be5a5e43b47a92a1a033067edd3e747c
yujunglove <dbwjdeor1234562@gmail.com>
2023-05-03 18:16:20 +0900

    commit

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -5,8 +5,28 @@ function Main() {
     return (
             <div className={MainCSS}>
             <div style={{ display: "flex" }}>
-                <div className="weather" style={{ flex: 1 }}>😚 오늘의 날씨는 짜자장 ~ </div>
-                <div className="todo" style={{ flex: 1 }}>todo</div>
+                <div className="weather" style={{ flex: 1 }}>😚 오늘의 날씨는 짜자장 ~</div> 
+                <div className="todo1" style={{ flex: 1 }}>
+                <div class="todoText1">할 일</div>
+                <div class="todoText0">할 일일</div>
+                <div class="todoNumber1">3</div>
+                 </div>
+
+                <div className="todo2" style={{ flex: 1 }}>
+                <div class="todoText2">A 구역 리프트 점검</div>
+                <div class="todoMinibar1">진행중</div>
+                <div class="todoMinibar2">장치관리</div>
+                <div class="todoMinibar3">물청소</div>
+                </div>
+
+                <div className="todo3" style={{ flex: 1 }}>
+                <div class="todoText3">새로운 파트너 관리 교육</div>
+                <div class="todoMinibar4">완료</div>
+                <div class="todoMinibar5">리프트 안전 교육</div>
+                <div class="todoMinibar6">관리</div>
+                </div>
+               
+
                 <div className="board" style={{ display: "flex", flex: 1 }}>
                 <div className="notic" style={{ flex: 1 }}>공지사항</div>
                 <div className="partBoard" style={{ flex: 1 }}>부서게시판</div>
--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className={ FooterCSS.footerDiv }>
             <h3 style={ { width: '100%', textAlign: 'center'} }>
-                Copyright 2023. Greedy All rights reserved.
+                Copyright 2023. SkyLift All rights reserved.
             </h3>
         </div>
     );
